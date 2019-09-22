e3389b746ab2aa9b89d78c11f8d27f7186e707f6
jasvirkaur <jasvir.kaur1224@gmail.com>
2019-09-21 23:15:02 -0400

    First commit

---
+++ src/main/java/com/example/exercise_practise/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.exercise_practise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView1, textView2, textView3, textView4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView1 = (TextView) findViewById(R.id.text1);
+        TextView textView2 = (TextView) findViewById(R.id.text2);
+        TextView textView3 = (TextView) findViewById(R.id.text3);
+        TextView textView4 = (TextView) findViewById(R.id.text4);
+
+        String X =
+                "copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss";
+
+        // Total Characters
+        String y = String.valueOf(X.length());
+        System.out.println(X.length());
+
+        textView1.setText(y);
+
+    }
+}
